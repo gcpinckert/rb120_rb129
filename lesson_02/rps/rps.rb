a7e4fd4a3c3198b64ef09136e45d430b8baa23af
Ginni Pinckert <gcpinckert@gmail.com>
2021-06-25 14:19:32 -0700

    RPS: Improve UE / output formatting

--- lesson_02/rps/rps.rb
+++ lesson_02/rps/rps.rb
@@ -1,7 +1,76 @@
-require 'pry'
+require 'io/console'
+
+module Printable
+  CSI = "\e["
+  @@rows, @@columns = IO.console.winsize
+  BORDER_LINE = "*" * @@columns
+  INSIDE_WIDTH = @@columns - 4
+  HORIZONTAL_LINE = "**#{' ' * INSIDE_WIDTH}**"
+
+  def window_too_small?
+    @@rows < 50
+  end
+  
+  def clear_screen
+    system("clear") || system("cls")
+  end
+
+  def print_border
+    $stdout.write "#{CSI}2;1H"
+    2.times {puts BORDER_LINE}
+    (@@rows - 5).times do
+      puts HORIZONTAL_LINE
+    end
+    2.times{puts BORDER_LINE}
+  end
+
+  def print_banner(message)
+    $stdout.write "#{CSI}4;1H"
+    message.each do |line|
+      puts "**#{line.center(INSIDE_WIDTH)}**"
+    end
+  end
+
+  def print_message(message)
+    message.each do |line|
+      left_margin
+      puts "#{line.ljust(@@columns - 8)}**"
+    end
+  end
+
+  def print_message_input(message)
+    print_message(message)
+    left_margin
+  end
+
+  def move_down_1
+    $stdout.write "#{CSI}1B"
+  end
+
+  def move_up_1
+    $stdout.write "#{CSI}1A"
+  end
+
+  def clear_1_above
+    move_up_1
+    puts HORIZONTAL_LINE
+    move_up_1
+  end
+
+  def left_margin
+    $stdout.write "#{CSI}6C"
+  end
+
+  def move_to_bottom
+    $stdout.write "#{CSI}#{@@rows};1H"
+  end
+end
 
 class Move
+  include Printable
+
   VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  
   @@move_history = Hash.new([])
 
   attr_reader :beats, :value
@@ -19,16 +88,24 @@ class Move
     @@move_history
   end
 
-  def self.display_history
+  def self.history
     player1, player2 = @@move_history.keys
+    table_headings = "Round".center(INSIDE_WIDTH / 3) +
+                     player1.center(INSIDE_WIDTH / 3) +
+                     player2.center(INSIDE_WIDTH / 3)
 
-    puts "MOVE HISTORY".center(60, '-')
-    puts "Round".ljust(10) + player1.center(25) + player2.center(25)
+    # history display headings
+    history = ["MOVE HISTORY", "",  table_headings, ""]
 
+    # results for each round
     (0...@@move_history[player1].size).each do |i|
-      puts "(#{i + 1})".ljust(10) + "#{@@move_history[player1][i]}".center(25) +
-           "#{@@move_history[player2][i]}".center(25)
+      round_line = "(#{i + 1})".center(INSIDE_WIDTH / 3) + 
+                   "#{@@move_history[player1][i]}".center(INSIDE_WIDTH / 3) +
+                   "#{@@move_history[player2][i]}".center(INSIDE_WIDTH / 3)
+      history << round_line
     end
+
+    history
   end
 
   def beats?(other_move)
@@ -87,7 +164,7 @@ class Player
   attr_accessor :move, :score, :name
 
   def initialize
-    set_name
+    @name = ''
     @move = nil
     @score = 0
   end
@@ -98,12 +175,17 @@ class Player
 end
 
 class Human < Player
+  include Printable
+
   def set_name
+    3.times {move_down_1}
     n = ''
     loop do
-      puts "What's your name?"
+      print_message_input ["What's your name?"]
       n = gets.chomp
       break unless n.empty?
+      print_message ["Must enter a name."]
+      move_down_1
     end
 
     self.name = n
@@ -112,10 +194,12 @@ class Human < Player
   def chose
     choice = nil
     loop do
-      puts "Please choose rock, paper, scissors, lizard, or spock:"
+      print_message_input ["Please make a choice: " \
+                          "rock, paper, scissors, lizard, or spock:"]
       choice = gets.chomp.downcase
       break if Move::VALUES.include? choice
-      puts "Invalid choice."
+      print_message ["Invalid choice."]
+      move_down_1
     end
 
     self.move = Move.return_subclass_instance(choice)
@@ -135,33 +219,59 @@ class Computer < Player
 end
 
 class RPSGame
+  include Printable
+
   MAX_SCORE = 10
 
   attr_accessor :human, :computer
+  attr_reader :messages
 
   def initialize
     clear_screen
-    display_welcome_message
     @human = Human.new
     @computer = Computer.new
   end
 
-  def clear_screen
-    system("clear") || system("cls")
+  def expand_window
+    print_banner(expand_window_message)
+    loop do
+      break if IO.console.winsize[0] >=50
+    end
+    clear_screen
+    @@rows, @@columns = IO.console.winsize
+    print_border
   end
 
-  def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
-    puts "The first player to win #{MAX_SCORE} games wins!"
+  def welcome_message
+    ["Welcome to Rock, Paper, Scissors, Lizard, Spock!", "",
+      "Scissors cuts Paper covers Rock crushes",
+      "Lizard poisons Spock smashes Scissors",
+      "decapitates Lizard eats Paper disproves",
+      "Spock vaporizes Rock crushes Scissors", "",
+      "The first player to win #{RPSGame::MAX_SCORE} games wins!"]
+  end
+
+  def expand_window_message
+    ["Please expand your terminal window for optimal " \
+      "experience"]
+  end
+
+  def reset_round
+    clear_screen
+    print_border
+    print_banner(welcome_message)
+    3.times {move_down_1}
   end
 
   def play_single_match
+    reset_round
     human.chose
-    clear_screen
     computer.chose
     update_scores!
-    display_moves
-    display_winner
+    2.times {move_down_1}
+    print_message(moves_message)
+    move_down_1
+    print_message(winner_message)
   end
 
   def update_scores!
@@ -169,17 +279,24 @@ class RPSGame
     computer.score += 1 if computer.move.beats?(human.move)
   end
 
-  def display_moves
-    puts "#{human.name} chose #{human.move}"
-    puts "#{computer.name} chose #{computer.move}"
+  def moves_message
+    ["#{human.name} chose #{human.move}",
+    "#{computer.name} chose #{computer.move}"]
   end
 
-  def display_winner
-    puts "#{human.name} won!" if human.move.beats?(computer.move)
-    puts "#{computer.name} won!" if computer.move.beats?(human.move)
-    puts "It's a tie!" if human.move.tie?(computer.move)
-    puts "The score is now: #{human.name} - #{human.score} " \
-         "#{computer.name} - #{computer.score}"
+  def winner_message
+    message = ["", "The current score:", "#{human.name} - #{human.score}",
+              "#{computer.name} - #{computer.score}"]
+
+    if human.move.beats?(computer.move)
+      message.prepend("#{human.name} won!")
+    elsif computer.move.beats?(human.move)
+      message.prepend("#{computer.name} won!")
+    else
+      message.prepend("It's a tie!")
+    end
+
+    message
   end
 
   def game_won?
@@ -190,29 +307,45 @@ class RPSGame
     answer = nil
 
     loop do
-      puts "Would you like to play again? (y/n)"
+      print_message_input ["Would you like to play again? (y/n)"]
       answer = gets.chomp.downcase
       break if ['y', 'n'].include? answer
-      puts "Invalid choice, please enter 'y' or 'n'"
+      print_message ["Invalid choice, please enter 'y' or 'n'"]
+      move_down_1
     end
 
     answer == 'y'
   end
 
-  def display_goodbye_message
-    puts "Thanks for playing! Goodbye!"
+  def goodbye_message
+    ["Thanks for playing! Goodbye!"]
   end
 
   def play
+    print_border
+    expand_window if window_too_small?
+    print_banner(welcome_message)
+    human.set_name
+    computer.set_name
     loop do
       loop do
         play_single_match
-        break if game_won?
+        2.times {move_down_1}
+        break if game_won? || !(play_again?)
+        reset_round
       end
-      Move.display_history
+      clear_screen
+      print_border
+      print_banner(Move.history)
+      2.times{move_down_1}
       break unless play_again?
+      reset_round
     end
-    display_goodbye_message
+    2.times{move_down_1}
+    print_message(goodbye_message)
+    move_to_bottom
+    sleep(2)
+    clear_screen
   end
 end
 
